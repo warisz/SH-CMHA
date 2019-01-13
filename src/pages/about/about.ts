1aa9068b293df41e750db5ad2e8c5a6c634573d1
Waris Z <wariszahoor0@gmail.com>
2019-01-13 14:37:23 -0500

    Final Update

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,54 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+    @ViewChild('barCanvas') barCanvas;
 
-  }
+    barChart: any;
 
-}
+    constructor(public navCtrl: NavController) {
+
+    }
+
+    ionViewDidLoad() {
+        var rand = Math.floor(Math.random()*33)+1;
+        this.barChart = new Chart(this.barCanvas.nativeElement, {
+
+            type: 'bar',
+            data: {
+                labels: ["M", "T", "W", "T", "F", "S", "Today"],
+                datasets: [{
+                    label: 'Levels',
+                    data: [12, 24, 7, 14, 8, 3, rand],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+
+        });
+      }
+    }
+  
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,14 +14,65 @@ export class HomePage {
   send(){
     this.navCtrl.parent.select(1);
   }
+  added1 = false;
+  added2=false;
+  added3=false;
+  added4=false;
+  added5=false;
+  score = 0;
+  indNum1 = 0;
+  indNum2 = 0;
+  indNum3 = 0;
+  indNum4 = 0;
+  indNum5 = 0;
 
-  answer(num){
-    if (num==1){
-      console.log("1");
+  q1(num){
+    this.indNum1 = 1;
+    if(this.added1 == false){
+      this.score+=num;
+      this.added1 = true;
     }
-    if (num==2){
-      console.log("2");
+    console.log(this.score);
+
+  }
+
+  q2(num){
+    this.indNum2 = 1;
+    if(this.added2 == false){
+      this.score+=num;
+      this.added2 = true;
     }
+    console.log(this.score);
+
+  }
+
+  q3(num){
+    this.indNum3 = 1;
+    if(this.added3 == false){
+      this.score+=num;
+      this.added3 = true;
+    }
+    console.log(this.score);
+
+  }
+
+  q4(num){
+    this.indNum4 = 1;
+    if(this.added4 == false){
+      this.score+=num;
+      this.added4 = true;
+    }
+    console.log(this.score);
+
+  }
+
+  q5(num){
+    this.indNum5 = 1;
+    if(this.added5 == false){
+      this.score+=num;
+      this.added5 = true;
+    }
+    console.log(this.score);
   }
 
 
